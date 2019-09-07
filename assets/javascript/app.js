625f059a2730c289f5def6532acada32ee22fbfb
Tyler Winters <maxdavin1984@gmail.com>
2019-09-06 23:07:20 -0700

    Added so much and tried to get working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,54 +10,108 @@
 // when the quiz is over, display the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page)
 // -play 21-epilogue (mm2 ending)
 
-var questionSection = []
-var answerChoices = []
+var gameStartAudio = new Audio('assets/audio/mega-man_stage_select.mp3')
+var correctAnswer = new Audio('assets/audio/mega-man_stage_clear.mp3')
+var incorrectAnswer = new Audio('assets/audio/mega-man_death.mp3')
 
-
-var gameQuestions = {
-    q1: {
+var gameQuestions = [
+    {
         question: 'Who created the first six robot masters?',
-        options: ['Dr. Wily', 'Dr. Light', 'Proto Man', 'The Government'],
-        answer: [1],
+        options: ['Dr. Wily', 'Dr. Light', 'Proto Man', 'The Government '],
+        answer: ['Dr. Light'],
     },
-    q2: {
+    {
         question: 'What weapon is able to defeat Metal Man in one shot?',
         options: ['Crash Bomb', 'Atomic Fire', 'Metal Blade', 'Star Crash'],
-        answer: [3]
+        answer: ['Metal Blade']
     },
-    q3: {
-        question: 'Proto Man was actually not created before Mega Man.',
+    {
+        question: 'Proto Man was not created before Mega Man.',
         options: ['true', 'false'],
-        answer: false
+        answer: ['false']
     },
-    q4: {
+    {
         question: 'When wielded by Wood Man, his Leaf shield can be penetrated by what weapon?',
         options: ['Metal Blade', 'Shadow blade', 'Hard Knuckle', 'Nothing'],
-        answer: [3]                
+        answer: ['Nothing']
     },
-    q5: {
+    {
         question: "Mega Man's canine companion, Rush, was created after facing Wily's own 8 Robot masters.",
         options: ['true', 'false'],
-        answer: true
+        answer: ['true']
     },
-    q6: {
+    {
         question: "Which weapon does the most damage to Wily's Alien Holograph form that Mega Man encountered the second time they battled?",
         options: ['Bubble Lead', 'Thunder Beam', 'Search Snakes', 'Pharaoh Shot'],
-        answer: [0]
+        answer: ['Bubble Lead']
     },
-    q7: {
-        question: "Doc Robot utilizes the program data of which robot masters?",
-        options: ['Metal Man, Air Man, Bubble Man, Quick Man, Crash Man, Flash Man, Heat Man, Wood Man', 'Needle Man, Magnet Man, Gemini Man, Hard Man, Top Man, Snake Man, Spark Man, Shadow Man', 'Bright Man, Toad Man, Drill Man, Pharaoh Man, Ring Man, Dust Man, Dive Man, Skull Man'],
-        answer: 0
+    {
+        question: "Doc Robot utilizes the program data of which robot master",
+        options: ['Bright Man', 'Flash Man', 'Toad Man', 'None of the above'],
+        answer: ['Flash Man']
     },
+]
+
+var correct = 0;
+var wrong = 0;
+var index = 0;
+var timer = 0;
+var interval;
+
+$('#start').on('click', function () {
+    gameStartAudio.play();
+    setTimeout(questionDisplay, 7000);
+    console.log("hit start", index);
+});
+
+$(document).on("click", "input", function () {
+    console.log("correct answer", gameQuestions[index].answer[0]);
+    console.log("checked answer", $("input:checked").val());
+    if (gameQuestions[index].answer[0] === $("input:checked").val())  { 
+        console.log("correct answer", index);
+        correct++;
+        correctAnswer.play();
+    }
+    else {
+        wrong++;
+        incorrectAnswer.play();
+        console.log('wrong answer', index);
+    }
+
+    // $('#question-container').hide();
+    setTimeout(questionDisplay, 6000);
+    index++;
+    console.log("index++", index);
+    // questionDisplay();
+    $('#question-container').hide(500);
+
+})
+
+function questionDisplay() {
+    $('#question-container').empty();
+    // $('#question-container').hide(500);
+    $('#question-container').show(500);
+    interval = setInterval(time, 1000 * 15)
+    var questionHeader = $('<h2>');
+    questionHeader.text(gameQuestions[index].question);
+    $('#question-container').append(questionHeader);
+    // console.log(gameQuestions[index].options[0]);
+    for (let i = 0; i < gameQuestions[index].options.length; i++) {
+        const element = gameQuestions[index].options[i];
+        var answerHeader = $('<input type=radio>');
+        answerHeader.val(element);
+        $('#question-container').append(answerHeader, element);
+    }
 };
 
-$('#start').on('click', function() {
-    $('#introPage').html('');
-    // $('#question-container').html(<h6>This is a test</h6>)
-})
+function time(){
+    console.log(timer);
+    timer--;
+    var timerDiv = $('#showTime');
+    timerDiv.text(timer);
+    // document.getElementById('showTime').text
 
-$('#question-container').on('click', function(){
-    // console.log(gameQuestions.q1.question);
-    $('#question-container').append(q1.question'Who created the first six robot masters?');
-})
+    if (timer <= 0){
+        clearInterval(interval);
+    }
+}
